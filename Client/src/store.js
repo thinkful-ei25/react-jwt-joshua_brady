0b263cf7d616430428b0fb413af927cd0cd17cb8
Joshua Adams <joshadams554@gmail.com>
2018-11-15 12:39:02 -0500

    saving point

--- Client/src/store.js
+++ Client/src/store.js
@@ -1,7 +1,6 @@
 import {createStore, applyMiddleware, combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form';
 import thunk from 'redux-thunk';
-import {loadAuthToken} from './local-storage';
 import authReducer from './reducers/auth';
 import protectedDataReducer from './reducers/protected-data';
 import {setAuthToken, refreshAuthToken} from './actions/auth';
@@ -16,11 +15,6 @@ const store = createStore(
 );
 
 // Hydrate the authToken from localStorage if it exist
-const authToken = loadAuthToken();
-if (authToken) {
-    const token = authToken;
-    store.dispatch(setAuthToken(token));
-    store.dispatch(refreshAuthToken());
-}
+
 
 export default store;
